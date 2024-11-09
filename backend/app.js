97212f7ac69c148cd57e1c9b135a8a95584b00bf
Bryan <bryan.how@live.com>
2024-11-09 15:01:36 +0800

    removed node_modules

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,16 @@
 const express = require("express");
 const app = express();
 const cors =  require('cors');
+const { Pool } = require("pg");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require('passport-local').Strategy;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(session({ secret: env("SESSION_SECRET"), resave: false, saveUninitialized: false }));
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Express app listening on port ${PORT}!`));
