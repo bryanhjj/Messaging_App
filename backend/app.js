89f966326fafda6b564dffb01d4e55e5fa208fef
Bryan <bryan.how@live.com>
2024-11-12 17:36:19 +0800

    completed userRoute

--- backend/app.js
+++ backend/app.js
@@ -11,20 +11,7 @@ app.use(cors());
 app.use(session({ secret: "SESSION_SECRET", resave: false, saveUninitialized: false }));
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/", usersRouter);
-
-// update the login and logout routes
-app.get("/", (req, res) => {
-    res.render("index", { user: req.user });
-});
-app.get("/log-out", (req, res, next) => {
-    req.logout((err) => {
-      if (err) {
-        return next(err);
-      }
-      res.redirect("/");
-    });
-});
+app.use("/users", usersRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Express app listening on port ${PORT}!`));
--- backend/routes/usersRoute.js
+++ backend/routes/usersRoute.js
@@ -3,10 +3,29 @@ const userController = require('../controllers/usersController');
 const usersRouter = Router();
 const passport = require("../config/passport");
 
-usersRouter.get(
-    "/users", 
-    passport.authenticate("local", { successRedirect: "/", failureRedirect: "/"}), 
-    userController.usersAllGet
+usersRouter.get( // get all users
+    "/", 
+    passport.authenticate("local", {failureRedirect: "/"}), 
+    userController.usersAllGet,
+);
+usersRouter.get( // search for specific user
+    "/:userId",
+    passport.authenticate("local", {failureRedirect: "/"}),
+    userController.usersSearchPost,
+);
+usersRouter.post( // new user sign-up
+    "/signup",
+    userController.usersCreatePost,
+)
+usersRouter.put( // udpate user details
+    "/:userId",
+    passport.authenticate("local", {failureRedirect: "/"}),
+    userController.usersUpdatePost,
+);
+usersRouter.delete( // delete a user account
+    "/:userId",
+    passport.authenticate("local", {failureRedirect: "/"}),
+    userController.usersDelete,
 );
 
 module.exports = usersRouter;
