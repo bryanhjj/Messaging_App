22b6c7a0b86a1ee5c29558acd74b774c9eed6201
Bryan <bryan.how@live.com>
2024-11-19 20:51:36 +0800

    fixed indentation

--- frontend/src/components/LoginForm/LoginForm.jsx
+++ frontend/src/components/LoginForm/LoginForm.jsx
@@ -3,24 +3,29 @@ import { useEffect, useState, Form, useLoaderData } from "react";
 // some mui icon
 
 export function LoginForm () {
-    <Form>
-        <label>
-            <span>Email: </span>
-            <input
-                    placeholder="Insert your email here."
-                    aria-label="Email"
-                    type="email"
-                    name="email"
-            />
-        </label>
-        <label>
-            <span>Password: </span>
-            <input
-                    placeholder="Insert your password here."
-                    aria-label="Password"
-                    type="text"
-                    name="email"
-            />
-        </label>
-    </Form>
+    return (
+        <Form>
+            <label>
+                <span>Email: </span>
+                <input
+                        placeholder="Insert your email here."
+                        aria-label="Email"
+                        type="email"
+                        name="email"
+                />
+            </label>
+            <label>
+                <span>Password: </span>
+                <input
+                        placeholder="Insert your password here."
+                        aria-label="Password"
+                        type="password"
+                        name="password"
+                />
+            </label>
+            <div>
+                <button type="submit">Login</button>
+            </div>
+        </Form>
+    );
 };
