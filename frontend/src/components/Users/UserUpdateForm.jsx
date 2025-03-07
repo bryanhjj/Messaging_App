263d203265c4904d73240374a1ae60432be9b09e
Bryan <bryan.how@live.com>
2025-03-07 23:53:14 +0800

    basic funtionality done for UserUpdateForm

--- frontend/src/components/Users/UserUpdateForm.jsx
+++ frontend/src/components/Users/UserUpdateForm.jsx
@@ -1,42 +1,47 @@
-import { useRef, useContext } from "react";
-import UserContext from "./UserContext";
+import { useRef, useContext, useState } from "react";
+import { UserContext } from "../Users/UserContext";
+import { useNavigate } from "react-router-dom";
 
 // get mui stuff in here
 
 export default function UserUpdateForm () {
-    const [user] = useContext(UserContext);
+    const [ input, setInput ] = useState("");
+    const [ user ] = useContext(UserContext);
     const updateForm = useRef(null);
+    const navigate = useNavigate();
+    const token = localStorage.getItem("token");
 
-    const handleOnSubmit = async () => {
-        try {
-            const formData = new FormData(updateForm.current);
-            const result = await fetch(`${API_URL}/users/${user.id}`, {
-                method: "PUT",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(formData),
-            });
-            if (!result.ok) {
-                throw new Error('Update failed');
-            };
-        } catch(err) {
-            console.log(err);
+    const handleOnSubmit = async (e) => {
+        e.preventDefault();
+        const result = await fetch(`${process.env.REACT_APP_API_URL}/users/${user.id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify({
+                email: input,
+            }),
+        });
+        if (!result.ok) {
+            throw new Error('Update failed');
+        } else {
+            alert("Profile has been successfully updated.");
+            navigate("/");
         };
     };
 
     return (
-        <form id="edit-form" ref={updateForm} onSubmit={handleOnSubmit}>
-            <label>
-                <span>E-mail: </span>
-                <input
+        <form id="edit-form" onSubmit={handleOnSubmit}>
+            <label for="email">E-mail:</label>
+            <input
                     placeholder="Insert your email here."
                     aria-label="Email"
                     type="email"
                     name="email"
                     defaultValue={user?.email}
-                />
-            </label>
+                    onChange={(e) => setInput(e.target.value)}
+            />
             <button type="submit">Confirm changes</button>
         </form>
     );
