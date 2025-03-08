0312fa20ad3ba3d8e593b60c0ff7777cfb337dbe
Bryan <bryan.how@live.com>
2025-03-08 21:45:11 +0800

    added user account deletion funtion

--- frontend/src/components/Users/UserDeleteForm.jsx
+++ frontend/src/components/Users/UserDeleteForm.jsx
@@ -1,26 +1,31 @@
-import { useNavigate, useContext } from "react";
-import UserContext from "./UserContext";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { UserContext } from "../Users/UserContext";
 
 // get mui stuff in here
 
 export default function UserDeleteForm () {
     const navigate = useNavigate();
     const [user] = useContext(UserContext);
+    const token = localStorage.getItem("token");
 
-    const handleOnSubmit = async() => {
-        try {
-            const result = await fetch(`${API_URL}/users/${user.id}`, {
-                method: "DELETE",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            });
-            if (!result.ok) {
-                throw new Error('Unexpected error');
-            };
-        } catch(err) {
-            console.log(err);
-        }
+    const handleOnSubmit = async (e) => {
+        e.preventDefault();
+        const result = await fetch(`${process.env.REACT_APP_API_URL}/users/${user.id}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        if (!result.ok) {
+            throw new Error('Unexpected error');
+        } else {
+            alert("Account deletion successful.");
+            localStorage.setItem("token", null);
+            localStorage.setItem("expirationTime", Date.now());
+            navigate("/login");
+        };
     };
 
     return (
