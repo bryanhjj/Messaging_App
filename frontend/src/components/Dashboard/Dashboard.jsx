b18fb4cb045069c99acc8cf51be2537277e137b7
Bryan <bryan.how@live.com>
2025-03-12 20:25:21 +0800

    css for Dashboard component

--- frontend/src/components/Dashboard/Dashboard.jsx
+++ frontend/src/components/Dashboard/Dashboard.jsx
@@ -1,12 +1,14 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
 import { Link } from "react-router-dom";
 import { SearchBar } from '../Search/Search';
+import { UserContext } from "../Users/UserContext";
 import './Dashboard.css';
 
 // mui stuff
 
 export default function Dashboard () {
     const [chatroom, setChatroom] = useState([]);
+    const [user] = useContext(UserContext);
     const token = localStorage.getItem("token");
 
     useEffect(() => {
@@ -27,21 +29,32 @@ export default function Dashboard () {
     }, []);
 
     return(
-        <div>
+        <div className="dashboard-container">
+
             <SearchBar />
+            
             {chatroom.length >= 1 ? (
-                <div>
-                    {chatroom.map((c) => {
-                        return (
-                            <li key={c.id}>
-                                <Link to={`chatroom/${c.id}`}>{c.createdAt}</Link>
-                            </li>
-                        );
-                    })}
+                <div className="chatroom-results">
+                    <h3>Active chatrooms: </h3>
+                    <div className="chatroom-container">
+                        {chatroom.map((c) => {
+                            return (    
+                                <Link to={`chatroom/${c.id}`} className="chatroom-card" key={c.id}>
+                                    {c.users.map((u) => {
+                                        {/* to show the other chatroom participant that's not the logged in user themselves */}
+                                        if (u.id != user.id) {
+                                            return u.username;
+                                        }
+                                    })}
+                                </Link>
+                            );
+                        })}
+                    </div>
                 </div>
             ) : 
-            <div>You are not in any chatrooms.</div>
+            <div className="chatroom-results">You are not in any chatrooms.</div>
             }
+
         </div>
     );
 };
