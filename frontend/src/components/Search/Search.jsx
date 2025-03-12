16ab066732c4abde14154e06fbfb16072f078e5c
Bryan <bryan.how@live.com>
2025-03-12 14:13:49 +0800

    css for Search component

--- frontend/src/components/Search/Search.jsx
+++ frontend/src/components/Search/Search.jsx
@@ -1,14 +1,14 @@
-import { useState, Form } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import "./Search.css";
 
 // mui stuff here
+import SearchIcon from '@mui/icons-material/Search';
 
 export const SearchBar = () => {
     const [input, setInput] = useState("");
     const [searchResults, setSearchResults] = useState([]); 
     const token = localStorage.getItem("token");
-    const navigate = useNavigate();
 
     const handleFetchData = async (value) => {
         await fetch(`${process.env.REACT_APP_API_URL}/users/searchName?username=${value}`, {
@@ -28,19 +28,25 @@ export const SearchBar = () => {
     }
 
     return (
-        <div>
-            <input 
-                placeholder="Search user"
-                value={input}
-                onChange={(e) => handleChange(e.target.value)}
-            />
-            {searchResults.map((item) => {
-                return (
-                    <Link key={item.id} to={`profile/${item.id}`}>
-                        {item.username}
-                    </Link>
-                )
-            })}
+        <div className="search-com-container">
+            <div className="input-wrapper">
+                <SearchIcon className="search-icon"/>
+                <input 
+                    className="search-input"
+                    placeholder="Search via username..."
+                    value={input}
+                    onChange={(e) => handleChange(e.target.value)}
+                />
+            </div>
+            <div className="search-result-list">
+                {searchResults.map((item) => {
+                    return (
+                        <Link key={item.id} to={`profile/${item.id}`} className="results">
+                            {item.username}
+                        </Link>
+                    )
+                })}
+            </div>
         </div>
     )
 };
